96fe13c7319fdb9a90fa4608c716d96b298f18fa
Salman Baig <salman.sfa@outlook.com>
2024-11-06 14:19:45 +0100

    mobile sharing

---
+++ Models/LearnerShareReqest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VERAExample.Models
+{
+    public class LearnerShareRequest
+    {
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        public string MobileNumber { get; set; }
+    }
+}
